6a2d42687bda79fcca6c62001dc9bf7dd83d04e1
NeedWings <136832174+NeedWings@users.noreply.github.com>
2023-11-02 16:45:38 +0300

    Add files via upload

--- cfg.py
+++ cfg.py
@@ -68,6 +68,8 @@ from eth_account import Account as ethAccount
 import uuid
 import decimal
 from os import getcwd
+from platform import system
+
 import os
 import base64
 from cryptography.fernet import Fernet
@@ -75,7 +77,6 @@ import getpass
 import hashlib
 import sys
 import socket
-import wmi
 from aiohttp import ClientSession
 import sys, os
 import inquirer
@@ -161,8 +162,8 @@ Ask all questions in our chat.
 """
 
 KEY = "CEy426oSSaOTWDPgtuKxm1nS2uWN_4-L_eyt0dmAr40="
-SETTINGS_PATH = getcwd() + '\\data\\'
-
+SETTINGS_PATH = getcwd() + '/data/'
+SETTINGS_PATH = SETTINGS_PATH.replace("\\", "/")
 CONTRACT_ADDRESS_PREFIX = str_to_felt('STARKNET_CONTRACT_ADDRESS')
 UNIVERSAL_DEPLOYER_PREFIX = str_to_felt('UniversalDeployerContract')
 
@@ -383,3 +384,37 @@ async def sleeping(address, error = False):
 
 true = True
 false = False
+
+
+CONTRACTS_FOR_DEPLOY = {
+    "merkly": [bytes.fromhex("60806040526000805461ffff1916905534801561001b57600080fd5b5060fb8061002a6000396000f3fe6080604052348015600f57600080fd5b506004361060325760003560e01c80630c55699c146037578063b49004e914605b575b600080fd5b60005460449061ffff1681565b60405161ffff909116815260200160405180910390f35b60616063565b005b60008054600191908190607a90849061ffff166096565b92506101000a81548161ffff021916908361ffff160217905550565b61ffff81811683821601908082111560be57634e487b7160e01b600052601160045260246000fd5b509291505056fea2646970667358221220666c87ec501268817295a4ca1fc6e3859faf241f38dd688f145135970920009264736f6c63430008120033"), []],
+    "blank": [b'', []],
+    "remix": [bytes.fromhex("608060405234801561001057600080fd5b50610150806100206000396000f3fe608060405234801561001057600080fd5b50600436106100365760003560e01c80632e64cec11461003b5780636057361d14610059575b600080fd5b610043610075565b60405161005091906100a1565b60405180910390f35b610073600480360381019061006e91906100ed565b61007e565b005b60008054905090565b8060008190555050565b6000819050919050565b61009b81610088565b82525050565b60006020820190506100b66000830184610092565b92915050565b600080fd5b6100ca81610088565b81146100d557600080fd5b50565b6000813590506100e7816100c1565b92915050565b600060208284031215610103576101026100bc565b5b6000610111848285016100d8565b9150509291505056fea2646970667358221220322c78243e61b783558509c9cc22cb8493dde6925aa5e89a08cdf6e22f279ef164736f6c63430008120033"),[
+	{
+		"inputs": [],
+		"name": "retrieve",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "num",
+				"type": "uint256"
+			}
+		],
+		"name": "store",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]]
+}
