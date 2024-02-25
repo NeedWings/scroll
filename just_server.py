e75c5e88191322f5091e414ae2c90ef2c8882df2
NeedWings <136832174+NeedWings@users.noreply.github.com>
2024-02-25 19:32:13 +0300

    change port

--- just_server.py
+++ just_server.py
@@ -380,7 +380,7 @@ def run_app():
 
     os.environ["last_request"] = str(int(time()))
 
-    app.run(host="127.0.0.1", port=5000)
+    app.run(host="127.0.0.1", port=5002)
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000)
+    app.run(host="127.0.0.1", port=5002)
